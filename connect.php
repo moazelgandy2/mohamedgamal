ca98bfa393e1463bee3a34aec902e0926e8c2d37
moazelgandy2 <56883008+moazelgandy2@users.noreply.github.com>
2022-10-03 00:14:53 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,19 @@
-
 <?php
+$host = 'login-do-user-12560860-0.b.db.ondigitalocean.com'; //188.166.....
+$user = 'doadmin';
+$pass = 'AVNS_OXtkLSFhlOIvZi-wEig';
+$db = 'login';
+$p = '25060';
 
-$conn = mysqli_connect("localhost", "root", "", "login");
 
-if (!$conn) {
-    echo "Connection Failed";
+$link = mysqli_connect('$host', '$user', '$pass', '$db',25060);
+
+if($conn)
+{
+	echo "Connection Successful";
 }
+else
+{
+	echo "Could not connect";
+}
+?>
