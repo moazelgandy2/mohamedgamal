abb6b9b9e16c9ab60a4312f13fba1aae6f3fd881
moazelgandy2 <56883008+moazelgandy2@users.noreply.github.com>
2022-10-03 00:06:21 +0200

    Update connnect.php

--- profile/connnect.php
+++ profile/connnect.php
@@ -3,8 +3,10 @@ $host = 'login-do-user-12560860-0.b.db.ondigitalocean.com'; //188.166.....
 $user = 'doadmin';
 $pass = 'AVNS_OXtkLSFhlOIvZi-wEig';
 $db = 'login';
+$p = '25060';
 
-$conn=mysqli_connect($host,$user,$pass,$db);
+
+$conn=mysqli_connect($host,$user,$pass,$p,$db);
 
 if($conn)
 {
