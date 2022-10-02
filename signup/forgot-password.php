3f7148af5ab672c52592eab7650abe27bf4c9087
moazelgandy2 <56883008+moazelgandy2@users.noreply.github.com>
2022-10-02 23:05:12 +0200

    Add files via upload

---
+++ signup/forgot-password.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleforget.css">
+    <title>Reset Password-Fourth Dimention</title>
+</head>
+<body>
+   
+    <div class="container">
+        <div class="text-box">
+            <p>RESET PASSWORD</p>
+            <h1>PLEAS CONTACT SUPPORT FOR HELP</h1>
+            <h6>support@mail.com</h6>
+            <div class="row">
+                <a href="../signup/index.php">SignIn &#x27F6</a>
+                <a href="../signup/register.php">SignUp &#x27F6</a>
+            </div>
+        <div class="background">
+        </div>
+        <!-- <div class="nav">
+            <img src="imgs/logo3.png" class="logo">
+        </div> -->
+</body>
+</html>
---
+++ signup/welcome.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+
+    if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 10)) {
+        // last request was more than 30 minutes ago
+        session_unset();     // unset $_SESSION variable for the run-time 
+        session_destroy();   // destroy session data in storage
+    }
+    $_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+
+    if (!isset($_SESSION['SESSION_EMAIL'])) {
+        header("Location: index.php");
+        die();
+    }
+
+    include 'config.php';
+
+    $query = mysqli_query($conn, "SELECT * FROM users WHERE email='{$_SESSION['SESSION_EMAIL']}'");
+
+    if (mysqli_num_rows($query) > 0) {
+        $row = mysqli_fetch_assoc($query);
+
+        echo "Welcome " . $row['name'] . " <a href='logout.php'>Logout</a>";
+    }
+?>
