bd61aa65853415230baba9334a886f0b8e95e4ab
moazelgandy2 <56883008+moazelgandy2@users.noreply.github.com>
2022-10-02 23:13:53 +0200

    Update config.php

--- signup/config.php
+++ signup/config.php
@@ -2,7 +2,7 @@
 
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "login");
+$conn = mysqli_connect("login-do-user-12560860-0.b.db.ondigitalocean.com", "doadmain", "AVNS_OXtkLSFhlOIvZi-wEig", "login");
 
 if (!$conn) {
     echo "Connection Failed";
