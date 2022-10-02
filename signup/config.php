67fe0f49819e4ffcaaa824b52befad7d4bbebeb7
moazelgandy2 <56883008+moazelgandy2@users.noreply.github.com>
2022-10-03 00:17:45 +0200

    Update config.php

--- signup/config.php
+++ signup/config.php
@@ -3,7 +3,6 @@ $host = 'login-do-user-12560860-0.b.db.ondigitalocean.com'; //188.166.....
 $user = 'doadmin';
 $pass = 'AVNS_OXtkLSFhlOIvZi-wEig';
 $db = 'login';
-$p = '25060';
 
 
 $link = mysqli_connect('$host', '$user', '$pass', '$db',25060);
