b2425b4b219054b3ba396c17d047623c0b562407
moazelgandy2 <56883008+moazelgandy2@users.noreply.github.com>
2022-10-05 17:05:00 +0200

    Update watch.php

--- profile/watch.php
+++ profile/watch.php
@@ -74,7 +74,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../profile/css/framework.css"/>
+    <link rel="stylesheet" href="profile/css/framework.css"/>
     <link rel="stylesheet" href="../profile/css/master.css"/>
     <link rel="stylesheet" href="../profile/css/all.min.css"/>
     <link rel="stylesheet" href="../profile/css/normalize.css"/>
